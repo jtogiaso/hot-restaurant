0ee13862b78ecdf48ba61523694111796ea1665c
MrSpahceman <palacc25@gmail.com>
2017-10-25 20:37:45 -0700

    expressout

--- Database/server.js
+++ Database/server.js
@@ -5,15 +5,17 @@
 
 // call the packages we need
 var express = require('express'); // call express
-var app = express(); // define our app using express
+// var app = express(); // define our app using express
 var bodyParser = require('body-parser');
+var mySql = require("mysql");
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080; // set our port
+var PORT = 8080; // set our port
+//var PORT = process.env.PORT || 8080; // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -21,7 +23,10 @@ var router = express.Router(); // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.send("high");
+    // res.json({ message: 'hooray! welcome to our api!' });
 });
-
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
 // more routes for our API will happen here
